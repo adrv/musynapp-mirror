ef75eefb49c8e834c6c3808abd0a26d30899bb78
Aleksey <dreval.aleksey@gmail.com>
2013-08-20 01:40:33 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Musynapp::Application.config.secret_key_base = '149daadf454fae17c0bf6cf074f2367704b297d14cf9f1b1330614b6b4b8022bc8e9aeb4965737dd078c7ea2c554f2c19cb4be68ff24cc6a476871c134a22e34'
