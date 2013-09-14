ece862cb7d99f27a53d9d3aace2dd869319f252d
Aleksey <dreval.aleksey@gmail.com>
2013-09-14 14:06:14 +0400

    Public page(upcoming shows/genres)

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,8 @@
 class Genre < ActiveRecord::Base
   has_many :bands
 
+  validates_uniqueness_of :title
+
   def to_s
     title
   end
--- app/models/show.rb
+++ app/models/show.rb
@@ -2,14 +2,27 @@ class Show < ActiveRecord::Base
   belongs_to :venue
   belongs_to :band
   
-  scope :completed, -> { includes(:band, :venue).where('bands.virtual = ? AND venues.virtual = ?', false, false) }
+  scope :completed, lambda { includes(:band, :venue).where('bands.virtual = ? AND venues.virtual = ?', false, false) }
+  scope :next, lambda { |count=10| order('dt DESC').limit count }
+  scope :by_genre, lambda { |genre| includes(band: :genre).where('genres.title = ?', genre)}
 
   def to_s
     name
   end
+
+  def info
+    "#{dt.strftime("%H:%M")} #{band.name} at #{venue.name}"
+  end
   
-  def self.next count
-    completed.order('dt DESC').limit count
+  # Produces output with format:
+  # { 'Classical' => [show1, show2, ...], ... }
+  def self.genres_map per_genre
+    result = {}
+    genres = Genre.pluck :title
+    genres.each do |genre|
+      result[genre] = Show.by_genre(genre).next(per_genre)
+    end
+    result
   end
   
 end
