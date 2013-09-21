09966c3063a13c81168904d707eb245c49b57e20
Aleksey <dreval.aleksey@gmail.com>
2013-09-21 20:06:00 +0400

    fixing small bugs

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -35,7 +35,7 @@ class BandsController < ApplicationController
   private
 
   def band_params
-    params.permit(:band).permit(:name, :genre_id, :description, :links, :primary_song_id,
+    params.require(:band).permit(:name, :genre_id, :description, :links, :primary_song_id,
                                  { songs_attributes: [:upload], images_attributes: [:upload] } )
   end
 
